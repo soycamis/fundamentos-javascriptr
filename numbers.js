a5884d34a7e6a2772c1413146ebf683be2396f8b
soycamis <soycamis@proton.me>
2025-07-14 15:38:28 -0600

    Numeros en Javascript

---
+++ numbers.js
@@ -0,0 +1,48 @@
+// Resumen de numeros en javascript.
+
+// Numeros enteros
+const myNumber = 1;
+
+// Numeros decimales
+const myNumber2 = 1.5;
+
+// Numeros negativos
+const myNumber3 = -1;
+
+// Numeros muy grandes
+const myNumber4 = 123456789012345678901234567890n;
+
+
+// Operaciones basicas con numeros
+const myNumber5 = 1 + 1;
+const myNumber6 = 1 - 1;
+const myNumber7 = 1 * 1;
+const myNumber8 = 1 / 1;
+
+//Operaciones tipo moduilo y potencia
+const myNumber9 = 1 % 1;
+const myNumber10 = 1 ** 1;
+
+// Operaciones avanzadas con el objeto Math
+const myNumber11 = Math.round(1.5);
+console.log(myNumber11);
+const myNumber12 = Math.ceil(1.5);
+console.log(myNumber12);
+const myNumber13 = Math.floor(1.5);
+console.log(myNumber13);
+const myNumber14 = Math.trunc(1.5);
+console.log(myNumber14);
+const myNumber15 = Math.pow(1, 1);
+console.log(myNumber15);
+const myNumber16 = Math.sqrt(1);
+console.log(myNumber16);
+const myNumber17 = Math.abs(-2);
+console.log(myNumber17);
+const myNumber18 = Math.sin(1);
+console.log(myNumber18);
+const myNumber19 = Math.cos(1);
+console.log(myNumber19);
+const myNumber20 = Math.tan(1);
+console.log(myNumber20);
+
+// Operaciones tipo asignacion
